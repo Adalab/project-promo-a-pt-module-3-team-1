0f20a92630a382cd8a3fb660a2b234f5c59597c0
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-01-27 13:41:39 +0100

    add onChange and function Input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,97 +1,250 @@
-import '../scss/App.scss'
+import "../scss/App.scss";
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState({
+    name: "",
+    slogan: "",
+    technologies: "",
+    demo: "",
+    repo: "",
+    desc: "",
+    autor: "",
+    job: "",
+    image: "https://genweb.upc.edu/ca/documentacio/cursos/creacio-i-personalitzacio-de-plantilles/img/user.jpg",
+    photo: "https://www.itmplatform.com/wp-content/uploads/33664005_m.jpg",
+  });
+
+  const handleProyectName = (event) => {
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+
+    setData({ ...data,[inputName]:inputValue });  
+    //pendiente de explicación
+  };
+
+  
+
   return (
     <div>
-      
-  <div className="container">
+      <div className="container">
+        <header className="header">
+          <a
+            className="header__brand"
+            href="./"
+            title="Haz click para volver a la página inicial"
+          >
+            <img
+              className="header__companyLogo"
+              src="./images/laptop-code-solid.svg"
+              alt="Logo proyectos molones"
+            />
+            <h1 className="header__title">Proyectos molones</h1>
+          </a>
+          <img
+            className="logoSponsor"
+            src="./images/adalab.png"
+            alt="Logo Adalab"
+          />
+        </header>
 
-    <header className="header">
-      <a className="header__brand" href="./" title="Haz click para volver a la página inicial">
-        <img className="header__companyLogo" src="./images/laptop-code-solid.svg" alt="Logo proyectos molones"/>
-        <h1 className="header__title">Proyectos molones</h1>
-      </a>
-      <img className="logoSponsor" src="./images/adalab.png" alt="Logo Adalab"/>
-    </header>
-    
-    <main className="main">
-      <section className="hero">
-        <h2 className="title">Proyectos molones</h2>
-        <p className="hero__text">Escaparate en línea para recoger ideas a través de la tecnología</p>
-        <a className="button--link" href="./">Ver proyectos</a>
-      </section>
-
-      <section className="preview">
-        <div className="projectImage"></div>
-        <article className="card">
-          <h2 className="card__projectTitle"><span className="card__projectTitle--text">Personal project card</span></h2>
-
-          <div className="card__author">
-            <div className="card__authorPhoto"></div>
-            <p className="card__job">
-              Full stack Developer
+        <main className="main">
+          <section className="hero">
+            <h2 className="title">Proyectos molones</h2>
+            <p className="hero__text">
+              Escaparate en línea para recoger ideas a través de la tecnología
             </p>
-            <h3 className="card__name">Emmelie Bjôrklund</h3>
-          </div>
-      
-          <div className="card__project">            
-            <h3 className="card__name">Elegant Workspace</h3>
-            <p className="card__slogan">Diseños Exclusivos</p>
-            <h3 className="card__descriptionTitle">Product description</h3>
-            <p className="card__description">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla, quos? Itaque, molestias eveniet laudantium adipisci vitae ratione</p>
+            <a className="button--link" href="./">
+              Ver proyectos
+            </a>
+          </section>
 
-            <div className="card__technicalInfo">
-              <p className="card__technologies">React JS - HTML - CSS</p>
-          
-              <a className="icon icon__www" href="#" title="Haz click para ver el proyecto online">
-                Web link
-              </a>
-              <a className="icon icon__github" href="#" title="Haz click para ver el código del proyecto">
-                GitHub link
-              </a>
-            </div>
-          </div>
-        </article>
-      </section>
-      <form className="addForm">
-        <h2 className="title">Información</h2>
-        <fieldset className="addForm__group">
-          <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
-          <input className="addForm__input" type="text" name="name" id="name" placeholder="Nombre del proyecto"/>
-          <input className="addForm__input" type="text" name="slogan" id="slogan" placeholder="Slogan"/>
-          <div className="addForm__2col">
-            <input className="addForm__input" type="url" name="repo" id="repo" placeholder="Repositorio"/>
-            <input className="addForm__input" type="url" name="demo" id="demo" placeholder="Demo"/>
-          </div>         
-          <input className="addForm__input" type="text" name="technologies" id="technologies" placeholder="Tecnologías"/>
-          <textarea className="addForm__input" type="text" name="desc" id="desc" placeholder="Descripción" rows="5"></textarea>
-        </fieldset>
-    
-        <fieldset className="addForm__group">
-          <legend className="addForm__title">Cuéntanos sobre la autora</legend>
-          <input className="addForm__input" type="text" name="autor" id="autor" placeholder="Nombre"/>
-          <input className="addForm__input" type="text" name="job" id="job" placeholder="Trabajo"/>
-        </fieldset>
-    
-        <fieldset className="addForm__group--upload">
-          <label htmlFor="image" className="button">Subir foto del proyecto</label>
-          <input className="addForm__hidden" type="file" name="image" id="image"/>
-          <label htmlFor="photo" className="button">Subir foto de la autora</label>
-          <input className="addForm__hidden" type="file" name="photo" id="photo"/>
-          <button className="button--large">Guardar proyecto</button>
-        </fieldset>
-        
-      </form>
-    </main>
+          <section className="preview">
+            <div className="projectImage"></div>
+            <article className="card">
+              <h2 className="card__projectTitle">
+                <span className="card__projectTitle--text">
+                  Personal project card
+                </span>
+              </h2>
 
-    <footer className="footer">
-      <img className="logoSponsor" src="./images/adalab.png" alt="Logo Adalab"/>
-    </footer>
+              <div className="card__author">
+                <div className="card__authorPhoto"></div>
+                <p className="card__job">Full stack Developer</p>
+                <h3 className="card__name">{data.name}</h3>
+              </div>
 
-  </div>
+              <div className="card__project">
+                <h3 className="card__name">Elegant Workspace</h3>
+                <p className="card__slogan">{data.slogan}</p>
+                <h3 className="card__descriptionTitle">Product description</h3>
+                <p className="card__description">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Nulla, quos? Itaque, molestias eveniet laudantium adipisci
+                  vitae ratione
+                </p>
 
+                <div className="card__technicalInfo">
+                  <p className="card__technologies">React JS - HTML - CSS</p>
+
+                  <a
+                    className="icon icon__www"
+                    href="#"
+                    title="Haz click para ver el proyecto online"
+                  >
+                    Web link
+                  </a>
+                  <a
+                    className="icon icon__github"
+                    href="#"
+                    title="Haz click para ver el código del proyecto"
+                  >
+                    GitHub link
+                  </a>
+                </div>
+              </div>
+            </article>
+          </section>
+          <form className="addForm">
+            <h2 className="title">Información</h2>
+            <fieldset className="addForm__group">
+              <legend className="addForm__title">
+                Cuéntanos sobre el proyecto
+              </legend>
+              <input
+                className="addForm__input"
+                type="text"
+                name="name"
+                id="name"
+                value={data.name}
+                placeholder="Nombre del proyecto"
+                onChange={handleProyectName}
+              />
+              <input
+                className="addForm__input"
+                type="text"
+                name="slogan"
+                id="slogan"
+                value={data.slogan}
+                placeholder="Slogan"
+                onChange={handleProyectName}
+              />
+              <div className="addForm__2col">
+                <input
+                  className="addForm__input"
+                  type="url"
+                  name="repo"
+                  id="repo"
+                  value={data.repo}
+                  placeholder="Repositorio"
+                  onChange={handleProyectName}
+                />
+                <input
+                  className="addForm__input"
+                  type="url"
+                  name="demo"
+                  id="demo"
+                  value={data.name}
+                  placeholder="Demo"
+                  onChange={handleProyectName}
+                />
+              </div>
+              <input
+                className="addForm__input"
+                type="text"
+                name="technologies"
+                id="technologies"
+                value={data.name}
+                placeholder="Tecnologías"
+                onChange={handleProyectName}
+              />
+              <textarea
+                className="addForm__input"
+                type="text"
+                name="desc"
+                id="desc"
+                value={data.name}
+                placeholder="Descripción"
+                rows="5"
+                onChange={handleProyectName}
+              ></textarea>
+            </fieldset>
+
+            <fieldset className="addForm__group">
+              <legend className="addForm__title">
+                Cuéntanos sobre la autora
+              </legend>
+              <input
+                className="addForm__input"
+                type="text"
+                name="autor"
+                id="autor"
+                value={data.name}
+                placeholder="Nombre"
+                onChange={handleProyectName}
+              />
+              <input
+                className="addForm__input"
+                type="text"
+                name="job"
+                id="job"
+                value={data.name}
+                placeholder="Trabajo"
+                onChange={handleProyectName}
+              />
+            </fieldset>
+
+            <fieldset className="addForm__group--upload">
+              <label htmlFor="image" className="button">
+                Subir foto del proyecto
+              </label>
+              <input
+                className="addForm__hidden"
+                type="file"
+                name="image"
+                id="image"
+                value={data.name}
+                onChange={handleProyectName}
+              />
+              <label htmlFor="photo" className="button">
+                Subir foto de la autora
+              </label>
+              <input
+                className="addForm__hidden"
+                type="file"
+                name="photo"
+                id="photo"
+                value={data.name}
+                onChange={handleProyectName}
+              />
+              <button className="button--large">Guardar proyecto</button>
+            </fieldset>
+          </form>
+        </main>
+
+        <footer className="footer">
+          <img
+            className="logoSponsor"
+            src="./images/adalab.png"
+            alt="Logo Adalab"
+          />
+        </footer>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+
+// name: "",
+//     slogan: "",
+//     technologies: "",
+//     demo: "",
+//     repo: "",
+//     desc: "",
+//     autor: "",
+//     job: "",
+//     image: "https://genweb.upc.edu/ca/documentacio/cursos/creacio-i-personalitzacio-de-plantilles/img/user.jpg",
+//     photo: "https://www.itmplatform.com/wp-content/uploads/33664005_m.jpg",
