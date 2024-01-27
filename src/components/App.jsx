e24a8d859ef21ed94fb44b3808ca04b31d2a0028
Mariabote <enbotedebote@gmail.com>
2024-01-27 15:32:37 +0100

    prueba subida de imagen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,8 @@ function App() {
     image: "",
     photo: "",
   });
+  
+
 
   const handleProyectName = (event) => {
     const inputName = event.target.name;
@@ -235,6 +237,8 @@ function App() {
 
 export default App;
 
+
+
 // name: "",
 //     slogan: "",
 //     technologies: "",
@@ -245,3 +249,82 @@ export default App;
 //     job: "",
 //     image: "https://genweb.upc.edu/ca/documentacio/cursos/creacio-i-personalitzacio-de-plantilles/img/user.jpg",
 //     photo: "https://www.itmplatform.com/wp-content/uploads/33664005_m.jpg",
+// Prueba de chat gpt
+// import React, { useState } from 'react';
+
+// function App() {
+//   const [data, setData] = useState({
+//     name: "",
+//     slogan: "",
+//     technologies: "",
+//     demo: "",
+//     repo: "",
+//     desc: "",
+//     autor: "",
+//     job: "",
+//     image: "",
+//     photo: "",
+//   });
+
+//   const handleInputChange = (event) => {
+//     const { name, value } = event.target;
+//     setData({
+//       ...data,
+//       [name]: value,
+//     });
+//   };
+
+//   const handleFileChange = (event) => {
+//     const file = event.target.files[0];
+//     const { name } = event.target;
+//     setData({
+//       ...data,
+//       [name]: file,
+//     });
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     // Aquí puedes enviar el formulario con los datos y las imágenes al servidor
+//     console.log('Datos del formulario:', data);
+//     // También puedes enviar las imágenes por separado si es necesario
+//     console.log('Imagen del proyecto:', data.image);
+//     console.log('Foto de la autora:', data.photo);
+//   };
+
+//   return (
+//     <div>
+//       {/* Tu código JSX anterior */}
+
+//       <form className="addForm" onSubmit={handleSubmit}>
+//         {/* Resto de tu formulario */}
+
+//         <fieldset className="addForm__group--upload">
+//           <label htmlFor="image" className="button">
+//             Subir foto del proyecto
+//           </label>
+//           <input
+//             className="addForm__hidden"
+//             type="file"
+//             name="image"
+//             id="image"
+//             onChange={handleFileChange}
+//           />
+//           <label htmlFor="photo" className="button">
+//             Subir foto de la autora
+//           </label>
+//           <input
+//             className="addForm__hidden"
+//             type="file"
+//             name="photo"
+//             id="photo"
+//             onChange={handleFileChange}
+//           />
+//           <button className="button--large">Guardar proyecto</button>
+//         </fieldset>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default App;
