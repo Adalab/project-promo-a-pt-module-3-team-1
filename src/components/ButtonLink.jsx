a1c634c0ecd790947a348f5709f2a919144fe243
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-19 00:25:05 +0100

    Link

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -1,6 +1,10 @@
-function ButtonLink () {
+// import { Link } from "react-router-dom";
 
+function ButtonLink() {
   return (
+    // <Link className="button--link" to="/">
+    //   Ver proyectos
+    // </Link>
     <a className="button--link" href="./LandingApp">
       Ver proyectos
     </a>
