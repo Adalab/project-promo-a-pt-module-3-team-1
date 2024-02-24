18cd1584cf5a013a05a4d885f27358c788b9d80b
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-02-24 13:42:52 +0100

    Add styles Landing

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
+// import LandingApp from './components/LandingApp.jsx'
 import { HashRouter} from 'react-router-dom';
 
 
